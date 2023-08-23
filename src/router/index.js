4050e8b35ee0e333bed920e4835c061bc9e4bc9d
Nabil-Alhanif <nabil.alhanif@protonmail.com>
2023-08-23 21:09:49 +0700

    Updating routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+	{
+		path: '/',
+		component: () => import('../views/Home.vue')
+	},
+	{
+		path: '/register',
+		component: () => import('../views/Register.vue')
+	},
+	{
+		path: '/sign-in',
+		component: () => import('../views/SignIn.vue')
+	},
+	{
+		path: '/feed',
+		component: () => import('../views/Feed.vue'),
+	}
+];
+
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            component: () => import('../views/Home.vue')
-        },
-        {
-            path: '/register',
-            component: () => import('../views/Register.vue')
-        },
-        {
-            path: '/sign-in',
-            component: () => import('../views/SignIn.vue')
-        },
-        {
-            path: '/feed',
-            component: () => import('../views/Feed.vue'),
-        }
-    ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+	routes
 })
 
 export default router
